eeb89ff225d4ce51c24cea1880a995511511ce1d
d86leader <d86leader@mail.com>
2018-02-03 14:43:02 +0700

    changed what gets put in a word

--- plugin/libclang.py
+++ plugin/libclang.py
@@ -444,7 +444,7 @@ def formatResult(result):
   if returnValue:
     menu = decode(returnValue.spelling) + " " + menu
 
-  completion['word'] = word
+  completion['word'] = abbr
   completion['abbr'] = abbr
   completion['menu'] = menu
   completion['info'] = info
